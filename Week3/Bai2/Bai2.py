8d077e87f122bf45b34de1820395ca5dfe8a644c
Mai Anh <anh.ttm193288@sis.hust.edu.vn>
2022-05-13 07:25:37 +0700

    complete Bai2

--- Week3/Bai2/Bai2.py
+++ Week3/Bai2/Bai2.py
@@ -8,40 +8,38 @@ def sigmoid(x):
 
 # Load data from csv file
 data = pd.read_csv('dataset.csv').values
-N, d = data.shape           # N - number of samples; d - number of feature
+N, d = data.shape
 x = data[:, 0:d-1].reshape(-1, d-1)
 y = data[:, 2].reshape(-1, 1)
 
 # Plot data with scatter
-x_accept = x[y[:,0]==1]
-x_reject = x[y[:,0]==0]
-plt.scatter(x_accept[:, 0], x_accept[:, 1], c='red', edgecolors='none', s=30, label='accept')
-plt.scatter(x_reject[:, 0], x_reject[:, 1], c='blue', edgecolors='none', s=30, label='reject')
+plt.scatter(x[:10, 0], x[:10, 1], c='red', edgecolors='none', s=30, label='accept')
+plt.scatter(x[10:, 0], x[10:, 1], c='blue', edgecolors='none', s=30, label='reject')
 plt.legend(loc=1)
 plt.xlabel('salary (million)')
-plt.ylabel('experience (years)')
+plt.ylabel('experience (year)')
 
-# Add column of ones to x
+# Add 1 column into  x
 x = np.hstack((np.ones((N, 1)), x))
 
-# Initial theta
+# Init theta
 w = np.array([0.,0.1,0.1]).reshape(-1,1)
 
-# Số lần lặp bước 2
+# Number of Iteration
 numOfIteration = 1000
 cost = np.zeros((numOfIteration,1))
 learning_rate = 0.01
 
-for i in range(1, numOfIteration):
-    
-	 # Tính giá trị dự đoán
+for i in range(1, numOfIteration):   
+	 # Calculate predict of y
     y_predict = sigmoid(np.dot(x, w))
     cost[i] = -np.sum(np.multiply(y, np.log(y_predict)) + np.multiply(1-y, np.log(1-y_predict)))
+
     # Gradient descent
     w = w - learning_rate * np.dot(x.T, y_predict-y)	 
     print(cost[i])
 
-# Vẽ đường phân cách.
+# Draw Sector
 t = 0.5
 plt.plot((4, 10),(-(w[0]+4*w[1]+ np.log(1/t-1))/w[2], -(w[0] + 10*w[1]+ np.log(1/t-1))/w[2]), 'g')
 plt.show()
